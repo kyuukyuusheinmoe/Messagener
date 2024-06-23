637354846d0f90a31cdcbafc726a83424746ebeb
kyuukyuusheinmoe <kyukyusheinmoe97@gmail.com>
2024-06-23 11:14:38 +0700

    reuse rabbitmq client options

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { rabbitMQConfig } from 'rabbitmq.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const rabbitMQUrl = configService.get<string>('RABBITMQ_URL');
-
-  const rabbitMQConfig: MicroserviceOptions = {
-    transport: Transport.RMQ,
-    options: {
-      urls: [rabbitMQUrl],
-      queue: 'messages_queue',
-      queueOptions: {
-        durable: false,
-      },
-    },
-  };
 
   app.connectMicroservice(rabbitMQConfig);
 
