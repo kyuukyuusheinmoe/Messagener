baca3a50ff0e2794bb1bb07c6bd449b3306a6df8
kyuukyuusheinmoe <kyukyusheinmoe97@gmail.com>
2024-06-23 11:27:57 +0700

    remove unused code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { rabbitMQConfig } from 'rabbitmq.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
 
   app.connectMicroservice(rabbitMQConfig);
 
